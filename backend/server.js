a68696da324a60e6ab427beffe2692182725bbdc
Tejas Kumar <tejas22052004@gmail.com>
2024-08-28 08:05:01 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 app.use(cors({
     origin: 'http://localhost:5173',
     methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
+    optionSuccessStatus:200,
     credentials: true
 }));
 
